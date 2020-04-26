6f5dd77e1eec6a26a3456ecb7851822cc130ff9a
Manish Khedekar <mankhedekar96@gmail.com>
2020-04-26 11:49:10 +0530

    Updates

---
+++ precache-manifest.1b682d36a03e517f5f7f9c72973f39e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93748713818e56d67729817ba22290b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f20b8acd3e03a60ff21",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8e0fb270e2adacc54d52",
+    "url": "/static/js/2.028ec7a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.028ec7a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f20b8acd3e03a60ff21",
+    "url": "/static/js/main.63dfe20d.chunk.js"
+  },
+  {
+    "revision": "25ae888276454a99378c",
+    "url": "/static/js/runtime-main.fa4ac668.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
